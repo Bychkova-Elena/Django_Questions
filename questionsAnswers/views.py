3a33d0b9443ff692afec6965c6307be31547ec5f
Elena Bychkova <lena.bychkova.200012@gmail.com>
2020-12-18 20:24:23 +0300

    DEV: categories are showed

--- questionsAnswers/views.py
+++ questionsAnswers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Category
+
+
+class CategoryView(View):
+    # Список категорий #
+    def get(self, request):
+        categories = Category.objects.all()
+        return render(request, "questionsAnswers/category_list.html", {"category_list": categories})
