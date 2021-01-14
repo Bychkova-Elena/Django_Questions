6ef80ef86f08692eaed5d25c235d3a03b6130290
Elena Bychkova <lena.bychkova.200012@gmail.com>
2021-01-14 22:46:42 +0300

    last changes

--- django_questions/settings.py
+++ django_questions/settings.py
@@ -146,8 +146,8 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 STATIC_URL = '/static/'
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_ROOT = 'static/'
 
 
 MEDIA_URL = '/media/'
